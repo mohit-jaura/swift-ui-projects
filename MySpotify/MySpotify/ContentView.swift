c563d751f7a50c2693186c7be47efd8512d681a2
Mohit Soni <mohit.soni@debutinfotech.com>
2024-01-09 14:19:00 +0530

    project init

---
+++ MySpotify/MySpotify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySpotify
+//
+//  Created by Mohit Soni on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySpotify/MySpotify/MySpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpotifyApp.swift
+//  MySpotify
+//
+//  Created by Mohit Soni on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MySpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
