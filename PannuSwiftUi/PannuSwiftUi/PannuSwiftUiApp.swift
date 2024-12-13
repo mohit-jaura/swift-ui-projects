b02afffb7b18d909303051d79acbf9ea559fc370
Mohit Soni <mosoni909@gmail.com>
2024-12-13 21:43:00 +0530

    project init

---
+++ PannuSwiftUi/PannuSwiftUi/PannuSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PannuSwiftUiApp.swift
+//  PannuSwiftUi
+//
+//  Created by Mohit Soni on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PannuSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PannuSwiftUi/PannuSwiftUiTests/PannuSwiftUiTests.swift
@@ -0,0 +1,17 @@
+//
+//  PannuSwiftUiTests.swift
+//  PannuSwiftUiTests
+//
+//  Created by Mohit Soni on 13/12/24.
+//
+
+import Testing
+@testable import PannuSwiftUi
+
+struct PannuSwiftUiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
